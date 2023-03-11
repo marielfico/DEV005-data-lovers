94715001fc88f626f412e177f15d248abc059c2a
IndiraPe <forever_tk_16@hotmail.com>
2023-03-11 10:15:09 -0500

    finaliza la HU n°1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,48 @@
-// estas funciones son de ejemplo
+import dataPkmn from './data/pokemon/pokemon.js';
 
-export const example = () => {
+//Pokemon en Top 10
+const topPkmn = dataPkmn.pokemon.map((x) => {
+  return {
+    nameTop: x.name,
+    cpTop: x.stats['max-cp'],
+    imgTop: x.img
+  } 
+});
+
+topPkmn.sort((a, b) => {
+  const cpA = a.cpTop;
+  const cpB = b.cpTop;
+  return cpB - cpA;
+});
+
+export const topPkmnSlice = topPkmn.slice(0, 10);
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+const nameP = dataPkmn.pokemon[0].name;
+const spanP = dataPkmn.pokemon[0]['spawn-chance'];
+export const top10 = (a, b) => {
+  
+  return a + b;
+};
+console.log(top10(nameP, spanP));
+
+/* export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+}; */
--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { top10, topPkmnSlice } from './data.js';
+import dataPkmn from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+//Pokemon en Top 10
+for (let i=0 ; i<topPkmnSlice.length; i++){
+  const pkmTop10 = document.getElementById(`${'img'+(i+1)}`);
+  pkmTop10.setAttribute("src", topPkmnSlice[i].imgTop);  
+  const pkmNameTop10 = document.getElementById(`${'name'+(i+1)}`);
+  pkmNameTop10.innerHTML = topPkmnSlice[i].nameTop.toUpperCase();
+}
+
+
+
+/* const pigdeyImg = document.getElementById('img11');
+const imgOne = dataPkmn.pokemon[0].img;
+pigdeyImg.setAttribute("src", imgOne);
+ */
+//console.log(dataPkmn.pokemon[0]['quick-move'][1].type);
+
+//console.log(dataPkmn.pokemon[0]['quick-move'][1].type);     normal
+//const prueba = data.top10(dataPkmn);
+
+//console.log(prueba);
+//console.log(example, dataPkmn);
+
+
+
+
+
+//console.log(top10);
+console.log(top10, dataPkmn);
